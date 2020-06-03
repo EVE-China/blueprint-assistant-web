c29fcf25f81e8ebccce06adb4ca316bcf79f8d27
Fan <xiaofanms@outlook.com>
2020-06-03 23:40:35 +0800

    提交代码。

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { Observable, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'blueprint-assistant-web';
+export class AppComponent implements OnInit {
+
+  title = 'EVE蓝图助手';
+
+  @ViewChild('bluePrintName') bluePrintNameInput: ElementRef;
+
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit() {
+    fromEvent(this.bluePrintNameInput.nativeElement, "change").subscribe(event => {
+      console.log(event);
+    });
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
